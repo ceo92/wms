e53236f854f95b0bb724dc2d82b03365689e7315
kinggora <jhha09@ajou.ac.kr>
2024-08-14 18:04:07 +0900

    feat: WarehouseType 도메인 추가

---
+++ src/domain/WarehouseType.java
@@ -0,0 +1,11 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WarehouseType {
+    private Integer id;
+    private String name;
+}
