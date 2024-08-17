df7fe93f8d3f4763755ec83a030e100238610c4a
kinggora <jhha09@ajou.ac.kr>
2024-08-18 01:52:50 +0900

    feat: WarehouseContract 도메인 추가

---
+++ src/domain/WarehouseContract.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class WarehouseContract {
+
+    private Integer id;
+    private Warehouse warehouse;
+    private BusinessMan businessMan;
+    private double capacity;
+    private LocalDate contractDate;
+    private int contractMonth;
+}
