1ffc45a2b60b0d9741451f4e110f0d21a1643274
Lee JuHwan <seungjoon0324@gmail.com>
2024-08-17 14:24:06 +0900

    feat: 공지사항 서비스 추가

---
+++ src/service/NoticeService.java
@@ -0,0 +1,11 @@
+package service;
+
+import dao.NoticeDao;
+import domain.Notice;
+import java.sql.SQLException;
+import java.util.List;
+
+public class NoticeService {
+
+  private final NoticeDao noticeDao = new NoticeDao();
+}
