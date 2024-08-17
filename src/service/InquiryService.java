d7b1f18da6160ccd970017fdc6770b5b60dea1b6
Lee JuHwan <seungjoon0324@gmail.com>
2024-08-17 14:13:29 +0900

    feat: addInquiry 공개 문의글 등록

--- src/service/InquiryService.java
+++ src/service/InquiryService.java
@@ -8,4 +8,9 @@ import java.util.List;
 public class InquiryService {
 
   private final InquiryDao inquiryDao = new InquiryDao();
+
+  public void addInquiry(String title, String content, String password) throws SQLException {
+    Inquiry inquiry = new Inquiry(title, content, false, password, 2, null); // roleId 2: 회원, guestId 없음
+    inquiryDao.save(inquiry);
+  }
 }
