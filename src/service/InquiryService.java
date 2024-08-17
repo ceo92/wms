469b998e100d3afa9cede71167a09db4c9b8fbdd
Lee JuHwan <seungjoon0324@gmail.com>
2024-08-17 14:12:52 +0900

    feat: InquiryService(문의글 서비스)

---
+++ src/service/InquiryService.java
@@ -0,0 +1,11 @@
+package service;
+
+import dao.InquiryDao;
+import domain.Inquiry;
+import java.sql.SQLException;
+import java.util.List;
+
+public class InquiryService {
+
+  private final InquiryDao inquiryDao = new InquiryDao();
+}
