7e07210e683bf29a0a9b0e3e937fb4a6335a774e
kinggora <jhha09@ajou.ac.kr>
2024-08-18 13:12:30 +0900

    remove: 컨트롤러, Main 삭제

--- src/controller/WarehouseManagerController.java
+++
@@ -1,6 +0,0 @@
-package controller;
-
-// WarehouseManagerController: 창고관리자 기능 관리 (AdminController와 동일)
-public class WarehouseManagerController extends AdminController {
-  // 창고관리자는 AdminController의 모든 기능을 사용
-}
