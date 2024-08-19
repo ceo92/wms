4bca810e540de47045bca5c33477f0d3f84deda0
Lee JuHwan <seungjoon0324@gmail.com>
2024-08-19 14:54:10 +0900

    refactor: WaybillService exception 변경

--- src/service/WaybillService.java
+++ src/service/WaybillService.java
@@ -16,28 +16,28 @@ public class WaybillService {
   private final DispatchDao dispatchDao = new DispatchDao();
 
   // 운송장 등록: 필요한 정보를 바탕으로 운송장 생성
-  public void registerWaybill(Waybill waybill) throws SQLException {
+  public void registerWaybill(Waybill waybill) {
     try {
       waybillDao.createWaybill(waybill);
     } catch (SQLException e) {
       System.out.println("운송장을 등록하는 중 오류가 발생했습니다: " + e.getMessage());
-      throw e;
+      throw new RuntimeException(e);
     }
   }
 
 
   // 운송장 리스트 조회: 모든 운송장 정보 조회
-  public List<Waybill> viewAllWaybills() throws SQLException {
+  public List<Waybill> viewAllWaybills() {
     try {
       return waybillDao.findAllWaybills();
     } catch (SQLException e) {
       System.out.println("운송장 리스트를 조회하는 중 오류가 발생했습니다: " + e.getMessage());
-      throw e;
+      throw new RuntimeException(e);
     }
   }
 
   // 운송장 수정: 운송장 정보 (기사님) 수정
-  public void modifyWaybill(int waybillId, DeliveryMan newDeliveryMan) throws SQLException {
+  public void modifyWaybill(int waybillId, DeliveryMan newDeliveryMan) {
     try {
       Waybill waybill = waybillDao.findWaybillById(waybillId);
       if (waybill != null) {
@@ -50,7 +50,7 @@ public class WaybillService {
       }
     } catch (SQLException e) {
       System.out.println("운송장 정보를 수정하는 중 오류가 발생했습니다: " + e.getMessage());
-      throw e;
+      throw new RuntimeException(e);
     }
   }
 }
