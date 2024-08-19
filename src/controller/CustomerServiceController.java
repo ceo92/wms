9e36e5e4adb043d0facce9709045366d0710f4c9
Lee JuHwan <seungjoon0324@gmail.com>
2024-08-19 16:04:39 +0900

    refactor: CustomerServiceController method 추가

--- src/controller/CustomerServiceController.java
+++ src/controller/CustomerServiceController.java
@@ -47,7 +47,7 @@ public class CustomerServiceController {
           manageNotices(br);
           break;
         case 2:
-          manageInquiries(br);
+          manageAdminInquiries(br);
           break;
         case 3:
           replyToPrivateInquiry(br);
@@ -184,7 +184,7 @@ public class CustomerServiceController {
   }
 
   //문의글 관리
-  private void manageInquiries(BufferedReader br) throws IOException, SQLException {
+  private void manageAdminInquiries(BufferedReader br) throws IOException, SQLException {
     System.out.println("1. 조회 2. 삭제 3. 종료");
     int choice = Integer.parseInt(br.readLine());
     switch (choice) {
@@ -201,6 +201,59 @@ public class CustomerServiceController {
       default:
         System.out.println("잘못된 선택입니다.");
     }
+  }
+
+  private void manageInquiries(BufferedReader br) throws IOException, SQLException {
+    System.out.println("1. 조회 2. 작성 3. 수정 4. 삭제 5. 종료");
+    int choice = Integer.parseInt(br.readLine());
+    switch (choice) {
+      case 1:
+        inquiryService.viewAllInquiries();
+        break;
+      case 2:
+        createInquiry(br);
+        break;
+      case 3:
+        updateInquiry(br);
+        break;
+      case 4:
+        deleteInquiry(br);
+        break;
+      case 5:
+        return;
+      default:
+        System.out.println("잘못된 선택입니다.");
+    }
+  }
+
+  private void createInquiry(BufferedReader br) throws IOException, SQLException {
+    System.out.print("제목: ");
+    String title = br.readLine();
+    System.out.print("내용: ");
+    String content = br.readLine();
+    System.out.print("비밀번호: ");
+    String password = br.readLine();
+    inquiryService.addInquiry(title, content, password);
+  }
+
+  private void updateInquiry(BufferedReader br) throws IOException, SQLException {
+    System.out.print("수정할 문의글 ID: ");
+    int id = Integer.parseInt(br.readLine());
+    System.out.print("비밀번호: ");
+    String password = br.readLine();
+    System.out.print("새 제목: ");
+    String title = br.readLine();
+    System.out.print("새 내용: ");
+    String content = br.readLine();
+    inquiryService.updateInquiry(id, title, content, password);
+  }
+
+  private void deleteInquiry(BufferedReader br) throws IOException, SQLException {
+    System.out.print("삭제할 문의글 ID: ");
+    int id = Integer.parseInt(br.readLine());
+    System.out.print("비밀번호: ");
+    String password = br.readLine();
+    inquiryService.deleteInquiry(id, password);
   }
 
   //비밀 문의글 댓글
